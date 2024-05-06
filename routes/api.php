20ef5895b580345cdadfa347556a53377878aca1
DeyvisonTav <deyvisontav@gmail.com>
2024-05-06 13:34:53 -0300

    feat: added controller auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return response()->json(['version-laravel' => app() -> version()]);
-});
+
+
+Route::group(['prefix'=> 'auth'], 
+    
+function ()  {
+    Route::post (uri:'login', action:'AuthController@login');
+    Route::get (uri:'logout', action:'AuthController@logout');
+    Route::get (uri:'refresh', action:'AuthController@refresh');
+    Route::get (uri:'me', action:'AuthController@me');
+}
+);
